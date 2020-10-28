11075ee8b708f84af998af3e09b3102669b9ded4
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2020-10-28 14:16:26 -0400

    Remove subtree lee

--- benchmarks/lee/lib/lee.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'lee/board'
-require_relative 'lee/adjacent'
-require_relative 'lee/cost'
-require_relative 'lee/matrix'
-require_relative 'lee/validate'
-require_relative 'lee/draw'
