a0d2de363c954e51cbd7debb7f2957150fd826f0
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2020-11-04 16:39:41 -0500

    Added harness to run liquid benchmark just once

---
+++ benchmarks/liquid-render/run_once.rb
@@ -0,0 +1,11 @@
+#require 'benchmark/ips'
+require_relative 'performance/theme_runner'
+
+Liquid::Template.error_mode = ARGV.first.to_sym if ARGV.first
+profiler = ThemeRunner.new
+
+profiler.compile
+
+puts "*** RUNNING BENCHMARK ***"
+
+profiler.render
