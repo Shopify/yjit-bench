b0f8db14208576ffe458841c4d8bbb2d443c7ba0
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2021-02-18 11:07:52 -0500

    Remove bootsnap dep. Add run_once.rb script for railsbench.

--- benchmarks/railsbench/Gemfile
+++ benchmarks/railsbench/Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.4.2', require: false
+#gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ benchmarks/railsbench/run_once.rb
@@ -0,0 +1,20 @@
+ENV['RAILS_ENV'] ||= 'production'
+require_relative 'config/environment'
+
+app = Rails.application
+
+possible_routes = ['/posts', '/posts.json']
+possible_routes.concat((1..100).map { |i| "/posts/#{i}"})
+
+visit_count = 2000
+rng = Random.new(0x1be52551fc152997)
+visiting_routes = Array.new(visit_count) { possible_routes.sample(random: rng) }
+
+visiting_routes.each do |path|
+  # The app mutates `env`, so we need to create one every time.
+  env = Rack::MockRequest::env_for("http://localhost#{path}")
+  response_array = app.call(env)
+  unless response_array.first == 200
+    raise "HTTP response is #{response_array.first} instead of 200. Is the benchmark app properly set up? See README.md."
+  end
+end
