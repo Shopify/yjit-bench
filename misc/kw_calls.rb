aa99908eee78685ec8f3d7338a5d5346383a0fa2
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2021-06-08 16:31:40 -0400

    Microbenchmark for keyword arguments

---
+++ misc/kw_calls.rb
@@ -0,0 +1,14 @@
+require "benchmark/ips"
+
+def foo_pos(a, b)
+    a + b
+end
+
+def foo_kw(a:, b:)
+    a + b
+end
+
+Benchmark.ips do |x|
+    x.report("call-pos") { foo_pos(1, 2) }
+    x.report("call-kw") { foo_kw(a: 1, b: 2) }
+end
