26bfd36c96c936a1a20b4341b916bc9dab092504
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2021-07-08 14:51:00 -0400

    Add run_once.sh script

--- benchmarks/optcarrot/run_once.rb
+++
@@ -1,7 +0,0 @@
-require_relative "lib/optcarrot"
-
-puts('*** RUNNING BENCHMARK ***')
-
-rom_path = File.join(__dir__, "examples/Lan_Master.nes")
-argv = ["--headless", "--frames", 200, "--no-print-video-checksum", rom_path]
-nes = Optcarrot::NES.new(argv).run
