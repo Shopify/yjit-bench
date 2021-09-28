406dcf9c0e58d4aa1c050cd3bb7a13e1264c1b5a
Kevin Newton <kddnewton@gmail.com>
2021-09-28 13:24:57 -0400

    Benchmark for keyword argument sends

---
+++ benchmarks/keyword_args.rb
@@ -0,0 +1,20 @@
+require 'harness'
+
+def add(left:, right:) = left + right
+
+run_benchmark(50) do
+    # 500K calls
+    500_000.times do |i|
+        # Call 10 times to reduce loop overhead, emphasize call performance
+        add(left: 1, right: 0)
+        add(left: 1, right: 1)
+        add(left: 1, right: 2)
+        add(left: 1, right: 3)
+        add(left: 1, right: 4)
+        add(left: 1, right: 5)
+        add(left: 1, right: 6)
+        add(left: 1, right: 7)
+        add(left: 1, right: 8)
+        add(left: 1, right: 9)
+    end
+end
