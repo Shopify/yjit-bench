6067c2e79e7cd6a214d02f40bff80605b72deca9
Noah Gibbs <noah.gibbs@shopify.com>
2021-10-05 16:07:15 +0100

    Add size verification of generated files.

--- benchmarks/hexapdf_line_wrap/benchmark.rb
+++ benchmarks/hexapdf_line_wrap/benchmark.rb
@@ -25,15 +25,20 @@ end
 # a lot of individual variant data.
 
 require "hexapdf"
+require "fileutils"
 
 IN_FILENAME = "odyssey.txt"
-OUT_FILENAME = "/tmp/bench-result.pdf"
-FONT_NAME = "./DejaVuSans.ttf"
 WIDTH = 50
 HEIGHT = 1000
 
+EXPECTED_MIN_SIZE = 569800
+EXPECTED_MAX_SIZE = 569900
+
 Dir.chdir __dir__
 
+Dir["/tmp/hexapdf-result*.pdf"].each { |file| FileUtils.rm file }
+
+index = 0
 run_benchmark(10) do
   ## TTF benchmark (v. slow)
   #HexaPDF::Composer.create(OUT_FILENAME, page_size: [0, 0, WIDTH, HEIGHT], margin: 0) do |pdf|
@@ -43,9 +48,17 @@ run_benchmark(10) do
   #end
 
   # Non-TTF benchmark
-  HexaPDF::Composer.create(OUT_FILENAME, page_size: [0, 0, WIDTH, HEIGHT], margin: 0) do |pdf|
+  index += 1
+  out_filename = "/tmp/hexapdf-result-#{ "%03d" % index }.pdf"
+  HexaPDF::Composer.create(out_filename, page_size: [0, 0, WIDTH, HEIGHT], margin: 0) do |pdf|
     pdf.text(File.read(IN_FILENAME), font_features: {kern: false},
              font: "Times", font_size: 10, last_line_gap: true,
              line_spacing: {type: :fixed, value: 11.16})
   end
+end
+
+Dir["/tmp/hexapdf-result*.pdf"].each do |file|
+  sz = File.stat(file).size
+  raise "Incorrect size #{sz} for file #{file}!" unless sz <= EXPECTED_MAX_SIZE && sz >= EXPECTED_MIN_SIZE
+  FileUtils.rm file
 end
