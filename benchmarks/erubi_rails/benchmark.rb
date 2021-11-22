05752b3ffa9a047ae5e1d271391d4dc58cca4869
Noah Gibbs <noah.gibbs@shopify.com>
2021-11-22 12:41:20 +0000

    Add an unused SECRET_KEY_BASE

--- benchmarks/erubi_rails/benchmark.rb
+++ benchmarks/erubi_rails/benchmark.rb
@@ -3,6 +3,8 @@ Dir.chdir __dir__
 use_gemfile
 
 ENV['RAILS_ENV'] ||= 'production'
+# The SECRET_KEY_BASE isn't used for anything, but we have to have one.
+ENV['SECRET_KEY_BASE'] = "1d1214a477334166ec542edb79047c7a042fb2b6dc90206d07b580615e0165c0371f365f20c93a06532b8462c11c0ce59da885734cb7b4e46805e2580b26ece5"
 require_relative 'config/environment'
 
 EXPECTED_TEXT_SIZE = 9369
