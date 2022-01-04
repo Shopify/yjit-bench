1751916ceb5de60311b6c632caed34f68d2a7982
Noah Gibbs <noah.gibbs@shopify.com>
2022-01-04 14:56:25 +0000

    Cache stubs between requests

--- benchmarks/erubi_rails/app/controllers/fake_discourse_controller.rb
+++ benchmarks/erubi_rails/app/controllers/fake_discourse_controller.rb
@@ -15,18 +15,19 @@ SiteSetting = ss_struct.new(false, company_struct.new(false), "settings title")
 class FakeDiscourseController < ApplicationController
     attr_reader :stub_assigns
 
-    def initialize
-        @stub_assigns = {}
+    def self.view_stub
+        return @stub_assignments if @stub_assignments
 
         # The topic_view seems to hold the details for *this* view of *this* topic.
         # It holds most of the interesting stubs for this view.
 
+        @stub_assignments = {}
         topic_struct = Struct.new(:id, :slug)
         topic = topic_struct.new(4321, "lunches_forever")
         topic_view_struct = Struct.new(:topic, :title, :posts, :url, :image_url, :post_custom_fields, :link_counts, :prev_page, :next_page, :read_time, :like_count, :published_time, :page_title, :print)
         topic_view_obj = topic_view_struct.new(topic, "Lunches Forever", [], "/fake/topic/url", "https://fake.image.url/fake.png", {}, {}, nil, nil, 0.0, 0, DateTime.now, "Viewing Page", false)
         def topic_view_obj.summary(*ignored); ""; end # Summary for meta tags
-        @stub_assigns[:topic_view] = topic_view_obj
+        @stub_assignments[:topic_view] = topic_view_obj
 
         # We'll create a user and some posts and then add them to the topic view object.
         user_struct = Struct.new(:name, :username)
@@ -38,9 +39,15 @@ class FakeDiscourseController < ApplicationController
 
         # Now add breadcrumbs and tags, which also get instance variables.
 
-        @stub_assigns[:breadcrumbs] = [ { url: "https://placeholder", color: "blue", name: "PageDivision" } ] * 3
+        @stub_assignments[:breadcrumbs] = [ { url: "https://placeholder", color: "blue", name: "PageDivision" } ] * 3
         tag_struct = Struct.new(:name)
-        @stub_assigns[:tags] = [ tag_struct.new("tag-name") ] * 3
+        @stub_assignments[:tags] = [ tag_struct.new("tag-name") ] * 3
+
+        @stub_assignments
+    end
+
+    def initialize
+        @stub_assigns = FakeDiscourseController.view_stub
     end
 
     # This is used as Discourse.base_url (see below)
