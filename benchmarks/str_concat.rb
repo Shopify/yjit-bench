a46f35c9ef57737f025ceff85e63f0bf57bb14b6
Noah Gibbs (and/or Benchmark CI) <noah.gibbs@shopify.com>
2022-04-28 13:38:42 +0000

    For str_concat microbench, test vs expected output instead of taking it on faith

--- benchmarks/str_concat.rb
+++ benchmarks/str_concat.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 require 'harness'
 
+NUM_ITERS = 10 * 1024
+TEST_STR = 'sssssséé'
+EXPECTED_OUTPUT = TEST_STR * NUM_ITERS
+OUTPUT = {}
+
 # Structuring this with a single method means we will not
 # get a single-encoding call site. That doesn't matter now,
 # but may matter with more-optimised generated code.
@@ -23,10 +28,19 @@ def concat_test
   # So far, binary versus UTF-8 encoding makes effectively no
   # difference in speed here. Observed diff is around 69.5 vs 68.9
   # iters/sec.
-  concat_single_test(10 * 1024, Encoding::UTF_8, 'sssssséé')
-  concat_single_test(10 * 1024, Encoding::BINARY, 'sssssséé')
+  OUTPUT[:one] = concat_single_test(NUM_ITERS, Encoding::UTF_8, TEST_STR)
+  OUTPUT[:two] = concat_single_test(NUM_ITERS, Encoding::BINARY, TEST_STR)
 end
 
 run_benchmark(100) do
   100.times { concat_test }
 end
+
+if OUTPUT[:one] != EXPECTED_OUTPUT
+  raise "Incorrect output for UTF-8 encoding!"
+end
+
+if OUTPUT[:two] != EXPECTED_OUTPUT
+  raise "Incorrect output for binary encoding!"
+end
+
