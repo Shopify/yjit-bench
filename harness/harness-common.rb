89f1808b7c395ee0a36e27dba39a71a4f8422258
Takashi Kokubun <takashikkbn@gmail.com>
2022-08-22 11:29:07 -0700

    Automatically ensure the shell-out assumption

--- harness/harness-common.rb
+++ harness/harness-common.rb
@@ -1,7 +1,8 @@
-# Ensure the ruby in PATH is the ruby running this, so we can safely shell out to other commands
-ruby_in_path = `ruby -e 'print RbConfig.ruby'`
-unless ruby_in_path == RbConfig.ruby
-  abort "The ruby running this script (#{RbConfig.ruby}) is not the first ruby in PATH (#{ruby_in_path})"
+# Ensure we can safely shell out to other commands
+ruby_in_path = proc { `ruby -e 'print RbConfig.ruby'` }
+unless ruby_in_path.call == RbConfig.ruby
+  ENV["PATH"] = "#{File.dirname(RbConfig.ruby)}:#{ENV["PATH"]}"
+  raise unless ruby_in_path.call == RbConfig.ruby
 end
 
 def run_cmd(*args)
