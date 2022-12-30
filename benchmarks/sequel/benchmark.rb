ffe62e17be69b580b2b75e6748d514066cc6f0bb
hmistry <hiren.mistry@chai-monsters.com>
2022-12-30 10:38:57 -0800

    Add benchmarks for Sequel +/- plugins

---
+++ benchmarks/sequel/benchmark.rb
@@ -0,0 +1,42 @@
+require "harness"
+require "securerandom"
+
+Dir.chdir __dir__
+use_gemfile
+
+require "sequel"
+
+# Base Sequel, no plugins
+DB = Sequel.sqlite
+
+DB.create_table :posts do
+  primary_key :id
+  String :title, null: false
+  String :body
+  String :type_name, null: false
+  String :key, null: false
+  Integer :upvotes, null: false
+  Integer :author_id, null: false
+  DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+  DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP
+end
+
+class Post < Sequel::Model; end
+
+50000.times {
+  Post.create(title: SecureRandom.alphanumeric(30),
+              type_name: SecureRandom.alphanumeric(10),
+              key: SecureRandom.alphanumeric(10),
+              body: SecureRandom.alphanumeric(100),
+              upvotes: rand(30),
+              author_id: rand(30))
+}
+
+# heat any caches
+Post.where(id: 1).first.title
+
+run_benchmark(10) do
+  1000.times do |i|
+    Post.where(id: i + 1).first.title
+  end
+end
