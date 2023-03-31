feb463570b875dc52b4cfff9a8df3b9d1c387326
Noah Gibbs <noah.gibbs@shopify.com>
2023-03-31 09:19:06 +0100

    Add harness that limits iterations and total time for MPLR paper

---
+++ harness-mplr/harness.rb
@@ -0,0 +1,30 @@
+require 'benchmark'
+require_relative "../harness/harness-common"
+
+# Minimum number of benchmarking iterations
+MAX_BENCH_ITRS = Integer(ENV.fetch('MAX_BENCH_ITRS', 1000))
+
+# Minimum benchmarking time in seconds
+MAX_BENCH_SECONDS = Integer(ENV.fetch('MAX_BENCH_SECONDS', 60 * 60))
+
+puts RUBY_DESCRIPTION
+
+# Takes a block as input
+def run_benchmark(_num_itrs_hint)
+  times = []
+  total_time = 0
+  num_itrs = 0
+
+  begin
+    time = Benchmark.realtime { yield }
+    num_itrs += 1
+
+    time_ms = (1000 * time).to_i
+    puts "itr \##{num_itrs}: #{time_ms}ms"
+
+    times << time
+    total_time += time
+  end until num_itrs >= MAX_BENCH_ITRS || total_time >= MAX_BENCH_SECONDS
+
+  return_results(times)
+end
