b70c98af2e29ea20aa86dabe52c2434608b188e8
Alan Wu <XrXr@users.noreply.github.com>
2023-06-21 12:55:06 -0400

    Add harness-once for quickly checking benchmarks
    It runs just the setup code of the benchmark and a single iteration.

---
+++ harness-once/harness.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+# This harness runs a single iteration of the benchmark and then returns.
+# For simplicity, the benchmark is _not_ timed.
+#
+# Intended only for checking whether the benchmark can set itself up properly
+# and can run to completion.
+
+require_relative '../harness/harness-common'
+
+def run_benchmark(_hint)
+  yield
+  return_results([], [0.001]) # bogus timing
+end
