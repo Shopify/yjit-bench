4cc24461b32500a0dc4abec6172ab9fb91fd0406
Takashi Kokubun <takashikkbn@gmail.com>
2023-07-27 10:14:10 -0700

    Add fluentd benchmark (#243)
    * Add fluentd benchmark
    
    * Add more variants to error messages

---
+++ benchmarks/fluentd/benchmark.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'harness'
+
+Dir.chdir(__dir__)
+use_gemfile
+
+require 'fluent/engine'
+require 'fluent/parser'
+
+# Prepare a fixture
+time = Time.new(2023, 7, 27, 9, 00, 00)
+errors = [
+  "Skipping user-data validation. No user-data found.",
+  "Python version change detected. Purging cache",
+  "No instance datasource found.",
+  "No kernel command line url found.",
+  "No local datasource found",
+]
+ltsv = 1000.times.map { |i| "time:#{time + i}	module:main.py	level:DEBUG	message:#{errors.sample}\n" }.join
+ltsv *= 1000
+
+# Prepare an LTSV parser
+parser = Fluent::Plugin::LabeledTSVParser.new
+parser.configure(Fluent::Config::Element.new('parse', '', {}, []))
+
+# Benchmark the `<parse>@type ltsv</parse>` use case
+run_benchmark(10) do
+  parser.parse(ltsv) {}
+end
