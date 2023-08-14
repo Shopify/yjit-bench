a358a0e4ea3453b4a622d1da30beb6045bbd5d77
Noah Gibbs <noah.gibbs@shopify.com>
2023-08-14 16:25:23 +0100

    Remove uglifier and assets-build settings, to remove execjs dependency

--- benchmarks/lobsters/config/environments/production.rb
+++ benchmarks/lobsters/config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Compress Javascript using a preprocessor.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
