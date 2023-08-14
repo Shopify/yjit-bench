8a3a4505161f48ab1bce6fac26cca96f17b209f7
Aaron Patterson <tenderlove@ruby-lang.org>
2023-08-14 09:55:00 -0700

    Add benchmarks for various GraphQL parsers
    This patch adds benchmarks for various GraphQL parsers.  The "graphql"
    and "tinygql" benchmarks are both pure Ruby, but "graphql" uses Racc
    which has some Ruby -> Native -> Ruby calls.  "graphql-native" is the
    same as "graphql" but uses a C extension

---
+++ benchmarks/graphql-native/benchmark.rb
@@ -0,0 +1,15 @@
+require "harness"
+
+Dir.chdir __dir__
+use_gemfile
+
+require "graphql"
+require "graphql/c_parser"
+
+file = File.read "negotiate.gql"
+
+run_benchmark(10) do
+  100.times do |i|
+    GraphQL.parse file
+  end
+end
