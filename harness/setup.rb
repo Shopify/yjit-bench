7145812514d21929073e4947e9d8e0218f58dfc4
Takashi Kokubun <takashikkbn@gmail.com>
2023-09-06 14:22:23 -0700

    Add a comment about -Iharness
    Co-authored-by: Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>

--- harness/setup.rb
+++ harness/setup.rb
@@ -1,3 +1,4 @@
 # Use harness/harness.rb by default. You can change it with -I option.
+# i.e. ruby -Iharness benchmarks/railsbench/benchmark.rb
 $LOAD_PATH << File.expand_path(__dir__)
 require "harness"
