7d777b0641034f742c51aa81c7814d08eb799935
Vinicius Stock <vinicius.stock@shopify.com>
2024-01-19 11:09:27 -0300

    Make `ruby-lsp` benchmark verify indexing performance

--- benchmarks/ruby-lsp/benchmark.rb
+++ benchmarks/ruby-lsp/benchmark.rb
@@ -5,31 +5,15 @@ require_relative "../../harness/loader"
 Dir.chdir(__dir__)
 use_gemfile
 
+# This benchmark checks the Ruby LSP indexing mechanism, which is used to keep track of all project declarations in
+# every file of a project and its dependencies
+
 require "ruby_lsp/internal"
 
-file_path = File.expand_path("fixture.rb", __dir__)
-file_uri = "file://#{file_path}"
-content = File.read(file_path)
+path = File.expand_path("fixture.rb", __dir__)
+index_path = RubyIndexer::IndexablePath.new(File.expand_path("../..", __dir__), path)
+content = File.read(path)
 
-rc_last = nil
-hl_last = nil
-
-# These benchmarks are representative of the three main operations executed by the Ruby LSP server
 run_benchmark(200) do
-  # File parsing
-  document = RubyLsp::Document.new(content)
-
-  # Running RuboCop related requests
-  rc = RubyLsp::Requests::Diagnostics.new(file_uri, document).run
-  rc_last = rc.size
-
-  # Running SyntaxTree visitor requests
-  hl = RubyLsp::Requests::SemanticHighlighting.new(
-    document,
-    encoder: RubyLsp::Requests::Support::SemanticTokenEncoder.new,
-  ).run
-  hl_last = hl.data.size
+  RubyIndexer::Index.new.index_single(index_path, content)
 end
-
-raise("ruby-lsp benchmark: the RuboCop diagnostics test is returning the wrong answer!") if rc_last != 34
-raise("ruby-lsp benchmark: the Semantic Highlighting test is returning the wrong answer!") if hl_last != 1160
--- benchmarks/ruby-lsp/fixture.rb
+++ benchmarks/ruby-lsp/fixture.rb
@@ -1,211 +1,272 @@
 # frozen_string_literal: true
 
-# Style mistakes such as indentation errors are intentional, so that the diagnostics request finds at least a few
-# violations
 
+# Important document line 1
+# Important document line 2
+# Important document line 3
+# Important document line 4
 module Blog
-  # :nodoc:
-  class User
+  extend T::Sig
+
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  module Articles
     extend T::Sig
 
-    # Set the right table name for this model
-  configure_model do |config|
-    T.bind(self, Model::Config)
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    class Post
+      extend T::Sig
 
-    config.table_name = "blog_users"
-      config.primary_key = :user_id
-      config.db = "blog"
-      config.adapter.with_defaults do |adapter_config|
-        adapter_config.name = :sqlite
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def initialize
+        @comments = []
       end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_1
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_2
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      attr_accessor :bogus_attribute_1, :bogus_attribute_2
     end
 
-    has_many :favorites, class_name: "Post"
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    class Comment
+      extend T::Sig
 
-    # Users are initialized with an email
-    # @param [String] email
-    # @return [User]
-    sig { params(email: String).void }
-    def initialize(email)
-      @email = email
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def initialize
+        @content = ""
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_1
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_2
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      attr_accessor :bogus_attribute_1, :bogus_attribute_2
     end
   end
 
-  # :nodoc:
-  class Comment
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  module Admin
     extend T::Sig
 
-    # Set the right table name for this model
-    configure_model do |config|
-      T.bind(self, Model::Config)
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    class User
+      extend T::Sig
 
-      config.table_name = "blog_comments"
-      config.primary_key = :comment_id
-      config.db = "blog"
-      config.adapter.with_defaults do |adapter_config|
-        adapter_config.name = :sqlite
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_1
       end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_2
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      attr_accessor :bogus_attribute_1, :bogus_attribute_2
     end
 
-    belongs_to :post
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    class Setting
+      extend T::Sig
 
-    # Comments are initialized with a user and a body
-    # @param [User] user
-    # @param [String] body
-    # @return [Comment]
-    sig { params(user: User, body: String).void }
-    def initialize(user, body)
-      @user = user
-      @body = body
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_1
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_2
+      end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      attr_accessor :bogus_attribute_1, :bogus_attribute_2
     end
   end
 
-  # :nodoc:
-  class Author
-    extend T::Sig
-    include SomeConcern
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  CONST_ALIAS = Articles
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  A, B, C = [1, 2, 3]
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  CONST_ALIAS::D = 4
 
-    LIST = T.let([
-      :foo,
-      :bar,
-      :baz,
-    ], T::Array[Symbol])
-
-    has_many :posts
-
-    validates :name, :subscribed, presence: true
-
-    enum status: { subscribed: 0, unsubscribed: 1 }
-
-    sig { returns(String) }
-    attr_reader :name
-
-    # Set the right table name for this model
-    configure_model do |config|
-      T.bind(            self, Model::Config)
-
-      config.table_name = "blog_authors"
-      config.primary_key = :author_id
-      config.db = "blog"
-      config.adapter.with_defaults do |adapter_config|
-        adapter_config.name = :sqlite
-      end
-    end
-
-    # Authors are initialized with a name
-    # @param [String] name
-    # @return [Author]
-    sig { params(name: String).void }
-    def initialize(name)
-      @name = name
-    end
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  sig { void }
+  def self.fake_method_1
   end
 
-  # :nodoc:
-  class Post
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  sig { void }
+  def self.fake_method_2
+  end
+
+  # Important document line 1
+  # Important document line 2
+  # Important document line 3
+  # Important document line 4
+  module MoreModules
     extend T::Sig
-    include SomeModule
 
-    CONSTANT = T.let(123, Integer)
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    class AnotherClass
+      extend T::Sig
 
-    belongs_to :author
-    has_many :comments
-    has_and_belongs_to_many :labels
-
-    validates :title, presence: true
-    validate :author_is_subscribed, if: -> { private? }
-
-    before_create :set_temporary_title
-
-    enum status: { draft: 0, published: 1, archived: 2 }
-    enum visibility: { public: 0, private: 1 }
-
-    scope :from_author, ->(author) { where(author: author) }
-    scope :visible, lambda do
-      where(visibility: :public)
-    end
-
-    sig {
-
-
-    returns(String) }
-    attr_reader :title
-
-    sig { returns(String) }
-    attr_reader :body
-
-    # Set the right table name for this model
-    configure_model do |config|
-      T.bind(self, Model::Config)
-
-      config.table_name = "blog_posts"
-      config.primary_key = :post_id
-      config.db = "blog"
-      config.adapter.with_defaults do |adapter_config|
-        adapter_config.name = :sqlite
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_1
       end
-    end
 
-    # Posts are initialized with a title and a body
-    # @param [String] title
-    # @param [String] body
-    # @return [Post]
-    sig { params(title: String, body: String).void }
-    def initialize(title, body)
-      @title = title
-      @body = body
-      @score = T.let(nil, T.nilable(Float))
-    end
-
-    # Find posts from the same author excluding this one
-    # @return [Array<Post>]
-    sig { returns(T::Array[Post]) }
-    def from_same_author
-      Post.where(author_id: author_id).not(id: id)
-    end
-
-    # Mark the post as published and notify all subscribers
-    # @return void
-    sig { void }
-    def publish!
-      published!
-      notify_subscribers
-    end
-
-    # Look for posts similar to this one
-    # @return [Array<Post>]
-    sig { returns(T::Array[Post]) }
-    def similar_posts
-      Post.where("title LIKE ?", "%#{title}%").select do |post|
-        (post.labels.map(&:name) - self.labels.map(&:name)).any?
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      sig { void }
+      def fake_method_2
       end
+
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      attr_accessor :bogus_attribute_1, :bogus_attribute_2
     end
 
-    # Calculates the post score based on the average score of all of its comments
-    # @return [Float]
-    sig { returns(Float) }
-    def score
-      @score ||= T.let(
-        comments.map(&:score).avg,
-        T.nilable(Float)
-      )
-    end
+    # Important document line 1
+    # Important document line 2
+    # Important document line 3
+    # Important document line 4
+    module NestedModule
+      extend T::Sig
 
-    private
+      # Important document line 1
+      # Important document line 2
+      # Important document line 3
+      # Important document line 4
+      class NestedClass
+        extend T::Sig
 
-    sig { void }
-    def author_is_subscribed
-      author.subscribed?
-    end
+        # Important document line 1
+        # Important document line 2
+        # Important document line 3
+        # Important document line 4
+        sig { void }
+        def fake_method_1
+        end
 
-    sig { void }
-    def notify_subscribers
-      NotifierJob.perform_later(self)
-    end
+        # Important document line 1
+        # Important document line 2
+        # Important document line 3
+        # Important document line 4
+        sig { void }
+        def fake_method_2
+        end
 
-    sig { void }
-    def set_temporary_title
-      self.title = "New post" if self.title.nil? && draft?
+        # Important document line 1
+        # Important document line 2
+        # Important document line 3
+        # Important document line 4
+        attr_accessor :bogus_attribute_1, :bogus_attribute_2
+      end
     end
   end
 end
