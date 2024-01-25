c91ccc105a8990e3febd70c952a99b8fa760d31a
Takashi Kokubun <takashikkbn@gmail.com>
2024-01-25 07:24:32 -0800

    Upgrade Rails to 7.1 (#278)
    * bundle update
    
    * bin/rails app:update
    
    * Load 7.1 defaults
    
    * Fix warnings and errors on benchmarks

--- benchmarks/erubi-rails/config/application.rb
+++ benchmarks/erubi-rails/config/application.rb
@@ -1,6 +1,18 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +21,12 @@ Bundler.require(*Rails.groups)
 module ErbTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
