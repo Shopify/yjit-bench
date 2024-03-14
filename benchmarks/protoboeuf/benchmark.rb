d5d234c2609bfb7aa4e7f524af5e087b0c631906
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2024-03-14 14:42:23 -0400

    Fix whitespace

--- benchmarks/protoboeuf/benchmark.rb
+++ benchmarks/protoboeuf/benchmark.rb
@@ -6,10 +6,6 @@ require_relative 'benchmark_pb'
 Dir.chdir __dir__
 fake_msg_bins = Marshal.load(File.binread('encoded_msg_bins.dump'))
 
-
-
 run_benchmark(20) do
-
   fake_msg_bins.each { |bin| ProtoBoeuf::ParkingLot.decode bin }
-
 end
