1c7effaefdd61f507a0ff803c9aa241d0e65c8c0
Randy Stauner <randy.stauner@shopify.com>
2024-09-12 08:34:33 -0700

    Don't let Rails auto-enable YJIT when benchmarking CRuby baseline

--- benchmarks/erubi-rails/config/environments/production.rb
+++ benchmarks/erubi-rails/config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # yjit-bench configurations
   config.active_record.sqlite3_production_warning = false
+  # If we want to benchmark with YJIT then it has already been enabled by command line arguments.
+  # If we are benchmarking CRuby without YJIT don't enable it even if this build has it.
+  config.yjit = false
 end
