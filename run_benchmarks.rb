ecfa743f87ed2ae1acec1b18e53f3636d86b0613
Randy Stauner <randy@r4s6.net>
2024-12-17 17:00:55 -0700

    Improve benchmark stability with cpusets, nice, and disabling of hyper threading

--- run_benchmarks.rb
+++ run_benchmarks.rb
@@ -11,6 +11,7 @@ require 'rbconfig'
 require 'etc'
 require 'yaml'
 require_relative 'misc/stats'
+require_relative 'misc/benchmark_mode'
 
 # Check which OS we are running
 def os
@@ -73,6 +74,13 @@ def set_bench_config(turbo:)
       at_exit { check_call("sudo -S sh -c 'echo 1 > /sys/devices/system/cpu/cpufreq/boost'", quiet: true) }
     end
     check_call("sudo -S cpupower frequency-set -g performance") unless performance_governor?
+  end
+
+  if os == :linux && ENV["BENCHMARK_MODE"] != '0'
+    BenchmarkMode.engage!&.then do |cpu|
+      ENV["YJIT_BENCH_CPU"] = cpu.to_s
+    end
+    at_exit { BenchmarkMode.disengage! }
   end
 end
 
@@ -261,8 +269,10 @@ def run_benchmarks(ruby:, ruby_description:, categories:, name_filters:, out_pat
       # Pin the process to one given core to improve caching and reduce variance on CRuby
       # Other Rubies need to use multiple cores, e.g., for JIT threads
       if ruby_description.start_with?('ruby ') && !no_pinning
-        # The last few cores of Intel CPU may be slow E-Cores, so avoid using the last one.
-        cpu = [(Etc.nprocessors / 2) - 1, 0].max
+        cpu = ENV.fetch("YJIT_BENCH_CPU") do
+          # The last few cores of Intel CPU may be slow E-Cores, so avoid using the last one.
+          [(Etc.nprocessors / 2) - 1, 0].max
+        end
         cmd += ["taskset", "-c", "#{cpu}"]
       end
     end
