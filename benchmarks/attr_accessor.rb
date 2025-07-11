38ae7837b0d8f84a80f30563064016dd6b617a0e
Max Bernstein <max@bernsteinbear.com>
2025-07-11 11:54:30 -0400

    Add attr_accessor benchmark

---
+++ benchmarks/attr_accessor.rb
@@ -0,0 +1,41 @@
+require_relative '../harness/loader'
+
+class TheClass
+  attr_accessor :levar
+
+  def initialize
+    @v0 = 1
+    @v1 = 2
+    @v3 = 3
+    @levar = 1
+  end
+
+  def get_value_loop
+    sum = 0
+
+    # 1M
+    i = 0
+    while i < 1000000
+      # 10 times to de-emphasize loop overhead
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      sum += levar
+      i += 1
+    end
+
+    return sum
+  end
+end
+
+obj = TheClass.new
+
+run_benchmark(850) do
+  obj.get_value_loop
+end
